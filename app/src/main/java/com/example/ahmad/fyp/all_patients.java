49b6b31cb8dbe4da721ba551fc2df9bb8ae3c6e5
MughalMirza <tiger.mirza99@gmail.com>
2017-10-14 16:12:36 +0500

    all patient front end and animation added

--- app/src/main/java/com/example/ahmad/fyp/all_patients.java
+++ app/src/main/java/com/example/ahmad/fyp/all_patients.java
@@ -3,9 +3,13 @@ package com.example.ahmad.fyp;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 /**
  * Created by ahmad on 10/13/2017.
@@ -16,7 +20,19 @@ public class all_patients extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.all_patients,container,false);
+
+        View patient =inflater.inflate(R.layout.all_patients,container,false);
+
+        CardView card = (CardView) patient.findViewById(R.id.card_view1);
+        CardView card2 = (CardView) patient.findViewById(R.id.card_view2);
+
+        Animation fade = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.myanimation);
+
+        card.startAnimation(fade);
+        card2.startAnimation(fade);
+
+
+        return patient;
     }
 
     @Override
